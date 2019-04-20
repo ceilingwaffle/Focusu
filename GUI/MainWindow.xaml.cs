3dde2fd2687ad5157a21ccf88b0a4da223588c2f
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-04-20 20:59:13 +1000

    Add automatic/manual radio buttons to control visibility of group boxes

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -25,6 +25,8 @@ namespace Focusu
         public MainWindow()
         {
             InitializeComponent();
+
+            AppState_RadioButton_Automatic.IsChecked = true;
         }
 
         private void AppState_Slider_FadeTiming_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -33,6 +35,18 @@ namespace Focusu
             this.fadeTime = TimeSpan.FromMilliseconds(ms);
             double seconds = e.NewValue / 1000;
             this.Label_FadeTiming.Content = $"{seconds:F2} seconds";
+        }
+
+        private void AppState_RadioButton_Manual_Checked(object sender, RoutedEventArgs e)
+        {
+            this.GroupBox_ManualControls.Visibility = Visibility.Visible;
+            this.GroupBox_AutomaticOptions.Visibility = Visibility.Collapsed;
+        }
+
+        private void AppState_RadioButton_Automatic_Checked(object sender, RoutedEventArgs e)
+        {
+            this.GroupBox_AutomaticOptions.Visibility = Visibility.Visible;
+            this.GroupBox_ManualControls.Visibility = Visibility.Collapsed;
         }
     }
 }
