3a80f69ed0cf2e4c35818d3a600c218c94a61ce6
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-05-02 18:20:13 +1000

    Minor changes to build info

--- Focusu.GUI/Properties/AssemblyInfo.cs
+++ Focusu.GUI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Focusu.GUI.Properties")]
+[assembly: AssemblyTitle("Focusu")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Focusu.GUI.Properties")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("Focusu")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
